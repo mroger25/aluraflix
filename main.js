aed765e3b7bc1f751fae037227f9b2839ff3be87
mroger25 <madsonmath@hotmail.com>
2021-09-16 18:18:02 -0300

    imagens, formatos e urls

--- main.js
+++ main.js
@@ -1,38 +1,28 @@
 let urlCapaMusicaLista = [
-  "https://i.ytimg.com/vi/5NV6Rdv1a3I/mqdefault.jpg",
-  "https://i.ytimg.com/vi/nlcIKh6sBtc/maxresdefault.jpg",
-  "https://i.ytimg.com/vi/IcrbM1l_BoI/maxresdefault.jpg",
-  "https://i.ytimg.com/vi/JRfuAukYTKg/hqdefault.jpg",
+  "https://i.ytimg.com/vi/5NV6Rdv1a3I/hq720.jpg",
+  "https://i.ytimg.com/vi/nlcIKh6sBtc/hq720.jpg",
+  "https://i.ytimg.com/vi/IcrbM1l_BoI/hq720.jpg",
 ];
 
 let tituloCapaMusicaLista = [
   "Daft Punk - Get Lucky ft. Pharrell Williams, Nile Rodgers",
   "Lorde - Royals",
   "Avicii - Wake Me Up",
-  "David Guetta - Titanium ft. Sia",
 ];
 
 let urlCapaMusicaListaTamanho = urlCapaMusicaLista.length;
 for (let i = 0; i < urlCapaMusicaListaTamanho; i++) {
+  // elementos
+  const imgElement = document.createElement("img");
+  const divElement = document.getElementsByClassName("crop")[i];
   // atributos
-  const titleCapaMusica = tituloCapaMusicaLista[i];
-  const srcCapaMusica = urlCapaMusicaLista[i];
-  const idCapaMusica = "img" + i;
-  const classCapaMusica = "crop";
+  const titleVideo = tituloCapaMusicaLista[i];
+  const srcVideo = urlCapaMusicaLista[i];
 
-  // criando o elemento img
-  const imgElement = document.createElement("IMG");
-  const divImagem = document.createElement("DIV");
-  // adicionando atributos no elemento criado
-  divImagem.setAttribute("class", classCapaMusica);
-  divImagem.setAttribute("id", idCapaMusica);
-  imgElement.setAttribute("src", srcCapaMusica);
-  imgElement.setAttribute("title", titleCapaMusica);
-  imgElement.setAttribute("alt", titleCapaMusica);
+  // configurando atributos
+  imgElement.setAttribute("src", srcVideo);
+  imgElement.setAttribute("title", titleVideo);
 
-  // selecionando o elemento (id="catalogo") que receberá o elemento criado
-  const imagens = document.getElementById("imagens");
-  // inserindo o elemento criado dentro do elemento selecionado
-  divImagem.appendChild(imgElement);
-  imagens.appendChild(divImagem);
+  // adicionando elemento
+  divElement.appendChild(imgElement);
 }
